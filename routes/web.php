37b509335499744d3d47926e572978bf73c43315
lijin <594314976@qq.com>
2017-01-19 16:47:01 +0800

    下载页

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ Route::get('/', function () {
     return redirect('admin');
 });
 
+Route::get('/download',function(){
+   return 'ng download page.Developing...';
+});
+
 Route::get('/test', 'TestController@index');
 
 Route::get('timestamp',function(){
