76c850bb3543416b88b03e07afb1c2e6b1f8728c
lijin <594314976@qq.com>
2017-05-12 11:08:46 +0800

    增加下载页

--- app/Http/Controllers/Api/ItemController.php
+++ app/Http/Controllers/Api/ItemController.php
@@ -60,6 +60,4 @@ class ItemController extends Controller
         }
         
     }
-    
-    
 }
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,6 @@ Route::get('/', function () {
     return redirect('admin');
 });
 
-Route::get('/download',function(){
-   return 'ng download page.Developing...';
-});
 Route::post('wechatpay/webNotice','Api\PayController@wxNotice');
 Route::post('alipay/webNotice','Api\PayController@aliNotice');
 
