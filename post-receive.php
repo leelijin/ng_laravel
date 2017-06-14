84184fd9f041504c4c76ade924b1f35a190671fb
lijin <594314976@qq.com>
2017-06-14 10:27:40 +0800

    test

--- post-receive.php
+++ post-receive.php
@@ -5,4 +5,11 @@
  * Date: 2017/6/14
  * Time: 9:34
  */
-exec('git pull origin master');
+//exec('git pull origin master');
+
+error_reporting ( E_ALL );
+$dir = '/home/wwwroot/ng_server/';//该目录为git检出目录
+$handle = popen('cd '.$dir.' && git pull origin master 2>&1','r');
+$read = stream_get_contents($handle);
+printf($read);
+pclose($handle);
