2f4111f7f7f37b4c3ff0e8d497814dc4b13fe154
lijin <594314976@qq.com>
2017-06-19 14:55:16 +0800

    联系我们修改

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,10 @@ Route::get('/', function () {
 });
 Route::get('contact_us', function () {
     $config_name = 'CONTACT_US_WEB';
-    return \Illuminate\Support\Facades\DB::table('config')->whereName($config_name)->value('value');
-    
+    $value= json_decode(\Illuminate\Support\Facades\DB::table('config')->whereName($config_name)->value('value'),true);
+    $data['img']=$_SERVER['HTTP_HOST'].\Illuminate\Support\Facades\DB::table('picture')->whereId($value[0])->value('path');
+    $data['content']=$value[1];
+    return $data;
 });
 Route::get('share', function () {
     return view('web.download');
