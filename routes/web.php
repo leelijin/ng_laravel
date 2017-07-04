9d075c8c42d2d5c16a3e25c5cd98c166c4fb90b7
lijin <594314976@qq.com>
2017-07-04 12:48:36 +0800

    后台删除功能

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use Maatwebsite\Excel\Facades\Excel;
 
 Route::get('/', function () {
     return redirect('master');
@@ -21,7 +20,29 @@ Route::post('wechatpay/webNotice','Api\PayController@wxNotice');
 Route::post('alipay/webNotice','Api\PayController@aliNotice');
 
 Route::get('importExcelJob/{file}',function($file){
-    dispatch(new App\Jobs\ImportExcelJob(App\Models\File::find($file)));
+    $fileInfo = App\Models\File::find($file);
+    
+    $excelPath = 'master/'.$fileInfo->savepath.$fileInfo->savename;
+    
+    $excelList =  Maatwebsite\Excel\Facades\Excel::load($excelPath)->getSheet(0)->toArray();
+    $rawList = collect($excelList)->map(function($item){
+        return array_slice($item,0,11);
+    });
+    $rawList->shift();
+    $rawList->each(function($item){
+        if($item[0] && $item[1] && $item[8]>0){
+            $list=[
+                'level_id'=>(int)$item[10],
+                'question'=>$item[0],
+                'content'=>$item[1],
+                'image1'=>(int)$item[2],
+                'image2'=>(int)$item[3],
+                'answer_options'=>[$item[4],$item[5],$item[6],$item[7]],
+                'right_answer'=>(int)($item[8]-1),
+            ];
+            App\Models\Question::create($list);
+        }
+    });
 });
 
 
