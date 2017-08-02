c9b9f3b62319ea7178a80adb1ec9924738f2d566
lijin <594314976@qq.com>
2017-08-02 09:28:50 +0800

    logs view

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,5 +1,6 @@
-@servers(['online'=>'120.77.71.171'])
+@servers(['online'=>'120.77.71.171','local'=>'127.0.0.1'])
 
-@task('deploy',['on'=>'online'])
+@task('deploy',['on'=>'local'])
+    cd /home/wwwroot/ng_server
     php artisan init:file
 @endtask
