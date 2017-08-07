fe952fe2559fe06c95c310b8df99888e2a7cd5cd
lijin <594314976@qq.com>
2017-08-07 10:27:26 +0800

    debug

--- app/Repository/FriendRepo.php
+++ app/Repository/FriendRepo.php
@@ -76,11 +76,11 @@ class FriendRepo
             $have = UserRepo::getUserSimpleInfo($v,function($query) use ($key){
                 $query->where('nickname','like','%'.$key.'%');
             });
-            if($key && !$have){
-                $list['data'][]=$have;
-            }else{
-                $list['data'][]=$have;
-            }
+            if($have)$list['data'][]=$have;
+        }
+        if($key){
+            $allLikeUser = User::simple()->whereStatus(1)->where('nickname','like','%'.$key.'%')->forPage($page,$limit);
+            $list['data'] = array_diff($allLikeUser,$list['data']);
         }
         $list['total']=count($list['data']);
         return $list;
