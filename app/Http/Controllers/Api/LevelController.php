807935b793e3ffc1251aa225e05c4c9ebaf65f0f
lijin <594314976@qq.com>
2017-09-13 17:32:28 +0800

    debug

--- app/Http/Controllers/Api/LevelController.php
+++ app/Http/Controllers/Api/LevelController.php
@@ -84,12 +84,14 @@ class LevelController extends Controller
         }
         
         $level_info = Level::gold()->find($gold_id);
-        if($request->has('client') && $request->get('client') == 'android'){
-            $notice = $level_info->getOriginal('notice');
-        }
-        $level_info = $level_info->toArray();
+        $notice = $level_info->getOriginal('notice');
         
-        $level_info['notice'] = $notice;
+        $level_info = $level_info->toArray();
+    
+        if($request->has('client') && $request->get('client') == 'android'){
+        
+            $level_info['notice'] = $notice;
+        }
         
         $level_info->num = LevelRepo::getLevelNum($gold_id,2);
         $gold_question_list = $model->inRandomOrder()->get();
